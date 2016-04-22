d48e7ad791120ca15137ddf3a7d628ee1d199a49
tore.brede@gmail.com <tore.brede@gmail.com>
2016-04-22 19:20:31 +0200

    Adding underscore for the time being at least. Setting up simple search function for parameter list.

--- ts/viewer/ParameterList.ts
+++ ts/viewer/ParameterList.ts
@@ -15,16 +15,20 @@ export class ParameterList {
     private searchText:string;
 
     // TODO Figure out why the Inject-annotation is needed here
-    constructor(@Inject(Backend) backend:Backend) {
+    constructor(@Inject(Backend) private backend:Backend) {
         this.parameters = backend.getParameters();
     }
 
-
+    
     updateSearchText(searchText:string):void {
         this.searchText = searchText;
+        this.parameters = [];
+        for(var parameterInfo of this.backend.getParameters()) {
+            if(parameterInfo.name.search(searchText) != -1) {
+                this.parameters.push(parameterInfo);
+            }
+        }
     }
-    
-    
 
     
 
--- tsd.d.ts
+++ tsd.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="ts/typings/raphael/raphael.d.ts" />
 /// <reference path="ts/typings/jquery/jquery.d.ts" />
+/// <reference path="typings/underscore/underscore.d.ts" />
